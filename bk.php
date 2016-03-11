cb163af10c91eace965fbfab9b8558a91359e5d1
cisvaibhav <vaibhav.d@cisinlabs.com>
2016-03-11 14:24:11 +0530

    Added files via upload

---
+++ bk.php
@@ -0,0 +1,8 @@
+sudo /etc/init.d/apache2 stop
+
+sudo /opt/lampp/lampp start
+
+
+1. http://www.bigtruckhoods.com/ DB:- bigtruck_ssq2
+2. http://www.bigtruckhoods.com/bigtruck_old/ DB:- bigtruck_ssq1
+3. http://www.bigtruckhoods.com/demo/ DB:- bigtruck_demo
